0d2e33b3ebbb681e7dd4578676e1e84def6266d6
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2021-04-26 21:15:07 +0530

    some minor upgrades

--- components/layout.js
+++ components/layout.js
@@ -12,6 +12,7 @@ export default function Layout({ children, home, title }) {
           as='font'
         />
         <title>{title}</title>
+        <meta name='viewport' content='width=device-width initial-scale=1.0' />
       </Head>
       <Navbar />
       <main>{children}</main>
