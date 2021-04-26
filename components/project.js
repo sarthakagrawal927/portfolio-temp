8a80e202adef3daee6a5c19f6830c183160b690c
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2021-04-26 20:22:07 +0530

    now responsive

--- components/project.js
+++ components/project.js
@@ -1,28 +1,30 @@
 const Project = ({ project }) => {
   return (
-    <div className='card col-1-of-3'>
-      <div className='card__side card__side--front'>
-        {" "}
-        <div className='card__picture'>&nbsp;</div>
-        <h4 className='card__heading'>
+    <div className='col-1-of-3'>
+      <div className='card'>
+        <div className='card__side card__side--front'>
           {" "}
-          <span className='card__heading-span'> {project.head}</span>
-        </h4>
-        <div className='card__details'>
-          <ul>
+          <div className='card__picture'>&nbsp;</div>
+          <h4 className='card__heading'>
             {" "}
-            {project.techStack.map((tech, index) => {
-              return <li key={index}>{tech}</li>;
-            })}
-          </ul>
+            <span className='card__heading-span'> {project.head}</span>
+          </h4>
+          <div className='card__details'>
+            <ul>
+              {" "}
+              {project.techStack.map((tech, index) => {
+                return <li key={index}>{tech}</li>;
+              })}
+            </ul>
+          </div>
         </div>
-      </div>
-      <div className='card__side card__side--back'>
-        {" "}
-        <div className='card__cta'>
-          <a href='' className='btn btn--white'>
-            <img src='/github-brands.svg' alt='' />
-          </a>
+        <div className='card__side card__side--back'>
+          {" "}
+          <div className='card__cta'>
+            <a href='' className='btn btn--white'>
+              <img src='/github-brands.svg' alt='' />
+            </a>
+          </div>
         </div>
       </div>
     </div>
