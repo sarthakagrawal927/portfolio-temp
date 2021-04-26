28beaf389ba045ae43f2aa2629ae005180acbff3
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2021-04-26 16:14:31 +0530

    fixed config file

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
 module.exports = {
-  amp: true,
-  images: {
-    domains: ["preview.redd.it", "external-preview.redd.it"],
-  },
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: "empty",
-      };
-    }
-
-    return config;
+  future: {
+    webpack5: true,
   },
 };
