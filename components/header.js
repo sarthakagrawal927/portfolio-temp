dd5207b869323e0c13f1789c0b724ba32c9250c1
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2021-07-28 23:35:51 +0530

    small changeS

--- components/header.js
+++ components/header.js
@@ -1,9 +1,9 @@
 const Header = () => {
   return (
     <header className='header'>
-      <div className='header__logo-box'>
+      {/* <div className='header__logo-box'>
         <img src='img/logo-white.png' alt='' className='header__logo' />
-      </div>
+      </div> */}
       <div className='header__text-box'>
         {" "}
         <h1 className='heading-primary'>
--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,16 @@ import Layout from "../components/layout";
 import Description from "../components/description";
 import Features from "../components/features";
 import Projects from "../components/projects";
-import Blogs from "../components/blogs";
+// import Blogs from "../components/blogs";
 import Contact from "../components/contact";
 export default function Home() {
   return (
-    <Layout title='Home | Significant Hobbies'>
+    <Layout title='Sarthak Agrawal'>
       <Header />
       <Description />
       <Features />
       <Projects />
-      <Blogs />
+      {/* <Blogs /> */}
       <Contact />
     </Layout>
   );
